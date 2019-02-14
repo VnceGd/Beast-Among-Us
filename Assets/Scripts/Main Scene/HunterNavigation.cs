71a40b812b2b23f31a7f26b8cd342ee410b6f886
Vincent Nguyen <vtn28@drexel.edu>
2019-02-14 17:43:38 -0500

    Prototype v0.3
    Add Bonus Scene

---
+++ Assets/Scripts/Main Scene/HunterNavigation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class HunterNavigation : MonoBehaviour
+{
+    private NavMeshAgent myAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAgent.destination = GameObject.Find("Player").transform.position;
+    }
+}
---
+++ Assets/Scripts/Main Scene/MainSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
